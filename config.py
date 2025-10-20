b482ba3b055751b56e0c2f8c9fdab6dc4b7dee1e
AkaGop <150795195+AkaGop@users.noreply.github.com>
2025-10-20 16:34:50 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,31 +1,4 @@
 # config.py
-"""
-Single source of truth for all static configuration data, including Alarm Classifications.
-"""
-CEID_MAP = {
-    # ... (content is the same as the last step)
-}
-RPTID_MAP = {
-    # ... (content is the same as the last step)
-}
-ALARM_MAP = {
-    # ... (content is the same as the last step)
-}
-
-# --- START OF HIGHLIGHTED CHANGE ---
-# This list defines which alarms are considered "hard faults" that stop the process.
-# All other alarms will be treated as "soft warnings."
-CRITICAL_ALARM_IDS = [
-    # System/Safety
-    1, 2, 190, 191, 192, 193, 194,
-    # HNC/Fork
-    4336, 4337,
-    # Panel Motion
-    1050, 1051, 1052, 1055, 5458, 5459, 5460,
-    # Timeouts that stop a process
-    5472, 5473, 5474, 5475, 5476, 5477,
-    # Inverter/Driver Errors
-    8192, 8193, 8194, 8195, 8196, 8197,
-    12288, 12289, 12290, 12291, 12292, 12293,
-]
-# --- END OF HIGHLIGHTED CHANGE ---
+CEID_MAP = {11:"Offline",12:"Local",13:"Remote",101:"Alarm Cleared",102:"Alarm Set",120:"ID Read",127:"Loaded to Tool",131:"Load Complete",132:"Unload Complete",136:"Mapping Complete",141:"Port Status Change",151:"Magazine Docked",180:"Request Dock",181:"Magazine Docked",182:"Magazine Undocked",183:"Request Operator Check",184:"Request Operator Login",185:"Request Mapping"}
+RPTID_MAP = {152:['OpID'],150:['MagID'],151:['PortID','MagID','OpID'],141:['PortID','PortStatus'],120:['LotID','PanelID'],121:['LotID','PanelID'],101:['AlarmID']}
+CRITICAL_ALARM_IDS=[1,2,190,191,192,193,194,4336,4337,1050,1051,1052,1055,5458,5459,5460]
