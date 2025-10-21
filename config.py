1b9f983bc4e5e82cd189256523914c6e155edf18
AkaGop <150795195+AkaGop@users.noreply.github.com>
2025-10-21 09:37:15 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,6 @@
 """
 Single source of truth for all static configuration data, including Alarm Classifications.
 """
-
 CEID_MAP = {
     11: "Equipment Offline", 12: "Control State Local", 13: "Control State Remote",
     16: "PP-SELECT Changed", 30: "Process State Change", 101: "Alarm Cleared",
@@ -13,24 +12,18 @@ CEID_MAP = {
     181: "MagazineDocked", 182: "MagazineUndocked", 183: "RequestOperatorIdCheck",
     184: "RequestOperatorLogin", 185: "RequestMappingCheck",
 }
-
 RPTID_MAP = {
-    152: ['Timestamp', 'OperatorID'],
-    150: ['Timestamp', 'MagazineID'],
+    152: ['Timestamp', 'OperatorID'], 150: ['Timestamp', 'MagazineID'],
     151: ['Timestamp', 'PortID', 'MagazineID', 'OperatorID'],
     141: ['Timestamp', 'PortID', 'PortStatus'],
     120: ['Timestamp', 'LotID', 'PanelID', 'Orientation', 'ResultCode', 'SlotID'],
     121: ['Timestamp', 'LotID', 'PanelID', 'SlotID'],
-    122: ['Timestamp', 'LotID', 'SourcePortID', 'DestPortID', 'PanelList'],
-    11:  ['Timestamp', 'ControlState'],
-    101: ['Timestamp', 'AlarmIDValue'],
+    11:  ['Timestamp', 'ControlState'], 101: ['Timestamp', 'AlarmIDValue'],
 }
-
 CRITICAL_ALARM_IDS = [
     1, 2, 190, 191, 192, 193, 194, 1050, 1051, 1052, 1055, 4336, 4337, 5458, 5459, 5460,
     8192, 8193, 8194, 8195, 8196, 8197, 12288, 12289, 12290, 12291, 12292, 12293,
 ]
-
 ALARM_MAP = {
     1: "CPU error", 2: "SafetyPLC error", 18: "Safety door open", 113: "HNC error",
     114: "Fork Coll.Detect error", 131: "Panel pick NG", 190: "Emergency stop",
